edcf8d575a96559856fc4921af99901699b6896c
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-03 18:24:24 +0500

    imp

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,3 +1,8 @@
+import 'package:d5_9_1/pages/cart_page.dart';
+import 'package:d5_9_1/pages/home_page.dart';
+import 'package:d5_9_1/pages/like_page.dart';
+import 'package:d5_9_1/pages/notification_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -8,8 +13,47 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final pages = [
+    HomePage(),
+    LikePage(),
+    CartPage(),
+    NotificationPage(),
+  ];
+  int currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: pages[currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentPage,
+        onTap: (index) {
+          currentPage = index;
+          setState(() {});
+        },
+        selectedItemColor: Color(0xffC67C4E),
+        unselectedItemColor: Color(0xffA2A2A2),
+        backgroundColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.house_fill),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.heart_fill),
+            label: "Like",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.cart_fill),
+            label: "Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: "Notification",
+          ),
+        ],
+      ),
+    );
   }
 }
