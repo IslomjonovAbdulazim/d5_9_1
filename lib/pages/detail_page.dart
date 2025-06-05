f5a6f4974386eeaea459b075d91c7fbdb43ffcbb
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-05 18:46:57 +0500

    imp

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -10,7 +10,8 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
-  String desc = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  String desc =
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
   bool isFull = false;
   String size = "M";
   List options = ["S", "M", "L"];
@@ -41,9 +42,12 @@ class _DetailPageState extends State<DetailPage> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(20),
+          padding: EdgeInsets.symmetric(horizontal: 20),
           child: ListView(
             children: [
+
+              // Homework .....
+
               Text(
                 "Description",
                 style: GoogleFonts.sora(
@@ -52,8 +56,78 @@ class _DetailPageState extends State<DetailPage> {
                   fontWeight: FontWeight.w600,
                 ),
               ),
-
-
+              Text(
+                desc,
+                style: GoogleFonts.sora(
+                  color: Color(0xffA2A2A2),
+                  fontWeight: FontWeight.w300,
+                  fontSize: 12,
+                ),
+                maxLines: isFull ? 20 : 3,
+                overflow: TextOverflow.ellipsis,
+              ),
+              CupertinoButton(
+                padding: EdgeInsets.zero,
+                onPressed: () {
+                  isFull = !isFull;
+                  setState(() {});
+                },
+                child: Text(
+                  isFull ? "Show Less" : "Show More",
+                  style: GoogleFonts.sora(
+                    color: Color(0xffC67C4E),
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Size",
+                style: GoogleFonts.sora(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Row(
+                children: options
+                    .map(
+                      (value) => Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 4),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Color(
+                                size == value ? 0xffC67C4E : 0xffA2A2A2,
+                              ),
+                            ),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: CupertinoButton(
+                            color: size == value
+                                ? Color(0xffC67C4E).withOpacity(.1)
+                                : Colors.white,
+                            onPressed: () {
+                              size = value;
+                              setState(() {});
+                            },
+                            child: Text(
+                              value,
+                              style: GoogleFonts.sora(
+                                color: size == value
+                                    ? Color(0xffC67C4E)
+                                    : Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
             ],
           ),
         ),
