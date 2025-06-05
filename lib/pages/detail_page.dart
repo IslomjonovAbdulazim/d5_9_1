9630a2c19aee2fe0dc43438b83e054eb48e71d1d
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-05 18:24:39 +0500

    imp

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -8,14 +10,49 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  String desc = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  bool isFull = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          "Detail",
+          style: GoogleFonts.sora(
+            fontSize: 20,
+          ),
+        ),
+        actions: [
+          CupertinoButton(
+            onPressed: () {},
+            child: Icon(
+              CupertinoIcons.heart,
+              size: 28,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(20),
-          child: Center(
-            child: Column(),
+          child: ListView(
+            children: [
+              Text(
+                "Description",
+                style: GoogleFonts.sora(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+
+
+            ],
           ),
         ),
       ),
